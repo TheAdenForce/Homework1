2079766ad139b2c1fb5d713c16ee1fb4a6aa08fb
Korolkov Ivan <ivan.korolkov3m17@mail.ru>
2024-07-31 15:25:23 +0300

    start programms

---
+++ src/masks.py
@@ -0,0 +1,29 @@
+def get_mask_card_number(card_number: str) -> str:
+    """ Разбиваем номер карты на части """
+    first_block = card_number[:4]
+    second_block = card_number[4:6]
+    fourth_block = card_number[12:] if len(card_number) > 12 else ""
+
+    """ Формируем замаскированный номер карты """
+    masked_card_number = f"{first_block} {second_block}** ****  {fourth_block}"
+
+    return masked_card_number
+
+
+def get_mask_account(account_number: str) -> str:
+    """ Берем последние 4 символа номера счета """
+    last_four_digits = account_number[-4:]
+
+    """ Формируем замаскированный номер счета """
+    masked_account_number = f"**{last_four_digits}"
+
+    return masked_account_number
+
+
+card_number = input("Введите номер карты")
+masked_card = get_mask_card_number(card_number)
+print(masked_card)
+
+account_number = input("Введите номер счета")
+masked_account = get_mask_account(account_number)
+print(masked_account)

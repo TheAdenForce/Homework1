a92be2211f4a1030c326be989364f42c53197c20
Korolkov Ivan <ivan.korolkov3m17@mail.ru>
2024-07-31 17:04:14 +0300

    первая функция ввода карты или счета

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,17 @@
 from masks import get_mask_card_number, get_mask_account
 
 card_or_account_request = input('Введите номер карты или счета: ')
+
+def mask_account_card(nums: str) -> str:
+    """ Функция принимает название платёжной системы и номер карты или номер счета и выводит их маскировку"""
+    if 'Счет' not in nums:
+        cards = get_mask_card_number(nums[-16:])
+        new_card = nums.replace(nums[-16:], cards)
+        return new_card
+    else:
+        account = get_mask_account(nums[-20:])
+        new_account = nums.replace(nums[-20:], account)
+        return new_account
+
+
+print(mask_account_card(card_or_account_request))

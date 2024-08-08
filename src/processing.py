3eb55569c4dada5e28d81aebbd659686524f29ba
Korolkov Ivan <ivan.korolkov3m17@mail.ru>
2024-08-08 11:06:16 +0300

    создание второй  функции

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,10 @@ def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
 
     """
     return [item for item in data if item.get('state') == state]
+
+def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
+    """
+    Сортирует список словарей по дате
+
+    """
+    return sorted(data, key=lambda x: x['date'], reverse=reverse)

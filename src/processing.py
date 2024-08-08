a89e91238112963de24d97d241fb15a6f0fd2bba
Korolkov Ivan <ivan.korolkov3m17@mail.ru>
2024-08-08 11:03:07 +0300

    Начало работы, создание первой функции

--- src/processing.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Dict
-
-
-def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
-    """
-    Фильтрует список словарей по значению ключа 'state'.
-
-    """
-    return [item for item in data if item.get('state') == state]

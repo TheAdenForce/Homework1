604ea4640c1342610d5b0f40c71c44e9476d5973
Korolkov Ivan <ivan.korolkov3m17@mail.ru>
2024-08-08 11:32:41 +0300

    доработка

--- src/processing.py
+++ src/processing.py
@@ -8,6 +8,7 @@ def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     """
     return [item for item in data if item.get('state') == state]
 
+
 def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
     """
     Сортирует список словарей по дате
@@ -15,13 +16,13 @@ def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
     """
     return sorted(data, key=lambda x: x['date'], reverse=reverse)
 
+
 data = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
-
 
 filtered_data_default = filter_by_state(data)
 filtered_data_canceled = filter_by_state(data, 'CANCELED')

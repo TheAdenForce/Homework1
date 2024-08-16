79e0029eab63cd600975d617c477bd7335404018
Korolkov Ivan <ivan.korolkov3m17@mail.ru>
2024-08-16 17:36:44 +0300

    создание фунций transaction_descriptions и  card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -12,3 +12,25 @@ def filter_by_currency(dict_list: List[Dict[str, Any]]) -> Iterator:
             raise KeyError
         else:
             break
+
+
+def transaction_descriptions(dict_transaction: List[Dict[str, Any]]) -> Iterator[str]:
+    """Функция для возврата описания транзакции"""
+    for info in dict_transaction:
+        if 'description' in info:
+            description = info["description"]
+            yield description
+        else:
+            raise KeyError
+
+
+def card_number_generator(start: int, stop: int) -> Iterator:
+    """Функция генерации случайных номеров банковских карт"""
+    while True:
+        numbers = random.randint(start, stop)
+        number_str = str(numbers)
+
+        while len(number_str) < 16:
+            number_str = "0" + number_str
+            formated = f"{number_str[0:4]} {number_str[4:8]} {number_str[8:12]} {number_str[12:]}"
+            yield formated

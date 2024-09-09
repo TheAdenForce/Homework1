0a26fb1af9866a3d997a710ba8329cabe86f07c3
Korolkov Ivan <ivan.korolkov3m17@mail.ru>
2024-09-09 18:11:17 +0300

    доработка и исправление ошибок

--- src/masks.py
+++ src/masks.py
@@ -10,22 +10,24 @@ card_number_logger = logging.getLogger()
 mask_account_logger = logging.getLogger()
 
 
-def get_mask_card_number(card_number: str) -> str:
-    """Функция возвращает замаскированный номер карты"""
-    if card_number.isdigit() and len(card_number) == 16:
-        masked_number = f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-        return masked_number
-    else:
-        raise ValueError("Ошибка! Номер карты должен состоять из 16 цифр без пробелов.")
+def get_mask_card_number(card_number: int) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску.
+    7000 79** **** 6361"""
+    card_number_logger.info("Создаю маску номера карты")
+    if len(str(card_number)) != 16:
+        card_number_logger.error("Неправильный номер карты")
+        raise ValueError("Неправильный номер карты")
+    card_number_logger.info("Маска номера карты создана")
+    return f"{int(str(card_number)[:4])} {int(str(card_number)[4:6])}** **** {int(str(card_number)[12:])}"
 
 
-def get_mask_account(account_number: str) -> str:
-    """Функция возвращает замаскированный номер аккаунта"""
-    if account_number.isdigit() and len(account_number) == 20:
-        masked_account = f"**{account_number[-4:]}"
-        return masked_account
-    else:
-        raise ValueError("Ошибка! Номер карты должен состоять из 20 цифр без пробелов.")
-
-
+def get_mask_account(account_number: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску.
+    **4305"""
+    mask_account_logger.info("Создаю маску номера счета")
+    if len(str(account_number)) != 20:
+        mask_account_logger.error("Неправильный номер счета")
+        raise ValueError("Неправильный номер счета")
+    mask_account_logger.info("Маска номера счета создана")
+    return f"**{int(str(account_number)[-4:])}"
 
